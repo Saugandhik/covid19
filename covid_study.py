3d3305fb42f2bf901e8a42c5c77cd7e48979f0b2
Saugandhik <saugandhik@gmail.com>
2020-06-07 23:15:51 +0530

    version 1

--- covid_study.py
+++ covid_study.py
@@ -6,7 +6,7 @@ Created on Sat Jun  6 20:48:11 2020
 """
 
 import requests
-import pandas as pd
+
 
 url = "https://covid-19india-api.herokuapp.com/all"
 
@@ -21,10 +21,22 @@ covid_data=response.json()
 covid_data_states=covid_data[1]['state_data']
 covid_data_total=covid_data[0]#dict
 
-df_states=pd.DataFrame(covid_data_states)
-df_total=pd.DataFrame(covid_data_total,index=['details'])
-print(df_states)
-print(df_total)
+
+def getstatesdata(state):
+    res=[x for x in covid_data_states if x['state'].lower().replace(" ","").strip()==state.lower().replace(" ","").strip()]
+    res_f=''
+    if len(res)>0:
+        for key,value in res[0].items():
+            res_f+=str(key)+" : "+str(value)+"\n"
+    return res_f
+
+def gettotaldata():
+    res_t=''
+    for key,value in covid_data_total.items():
+        res_t+=str(key)+" : "+str(value)+"\n"
+    return res_t
+
+
 
 
 
